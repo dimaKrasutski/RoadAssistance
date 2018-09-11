a54992c923771f18987b862e507be1033604499f
dimaKrasutski <krasutski.dmitri@gmail.com>
2018-09-11 19:49:58 +0300

    'frf'

--- router/problemRequest.js
+++ router/problemRequest.js
@@ -256,12 +256,7 @@ router.post('/helper_change_position',function (req,res) {
                         })
                         }}
             })
-
         })
-
-
-
-
     })
 });
 
@@ -282,7 +277,7 @@ router.get('/ebis_vse_konem', function (req, res) {
     User.findById(req.headers['uid'], function (err, user) {
         if (err) return res.status(500).send({message:'Error on the server'});
         if (!user) return res.status(404).send({message:'No user found'});
-        SendFcm(user.deviceIdFcmToken,"wdeded",user._id);
+        SendFcm(user.deviceIdFcmToken,"wdeded",user._id.toString());
         res.status(200).json({message:"vse zaebis"});
     })
 });
