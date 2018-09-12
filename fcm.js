063ae707a1ce0db54705f8f961883e94954e8088
dimaKrasutski <krasutski.dmitri@gmail.com>
2018-09-12 22:59:27 +0300

    'rfrf

--- fcm.js
+++ fcm.js
@@ -11,8 +11,7 @@ module.exports = function (token,msg,uid) {
     let payLoad = {
         data:{
             message: msg,
-            myKey: uid,
-            image: "https://www.google.by/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwja2o_AmrbdAhXR1qQKHfaWBx4QjRx6BAgBEAU&url=https%3A%2F%2Fwww.vanityfair.com%2Fnews%2Fpolitics%2F2012%2F07%2Fbenjamin-netanyahu-on-israel-mitt-romney&psig=AOvVaw1pKLemwa2o4-9tFKqM2Smh&ust=1536867162318679"
+            myKey: uid
         }
     };
 
